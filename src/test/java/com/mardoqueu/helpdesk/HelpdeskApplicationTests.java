5d3cf81367b908db4e9c33e48a0d42f342e7e898
Mardoqueu <mdq767@gmail.com>
2022-03-25 13:38:08 -0300

    projeto iniciado

---
+++ src/test/java/com/mardoqueu/helpdesk/HelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mardoqueu.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
