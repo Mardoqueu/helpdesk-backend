b142b7c2cd77dd88fca2ccfa6d1a217e53dea209
Mardoqueu <mdq767@gmail.com>
2022-03-25 13:54:51 -0300

    tipos enumerados criados

---
+++ src/main/java/com/mardoqueu/helpdesk/domain/enums/Prioridade.java
@@ -0,0 +1,33 @@
+package com.mardoqueu.helpdesk.domain.enums;
+
+public enum Prioridade {
+
+	BAIXA(0, "BAIXA"), MEDIA(1, "MEDIA"), ALTA(2, "ALTA");
+	
+	private Integer codigo;
+	private String descricao;
+	private Prioridade(Integer codigo, String descricao) {
+		this.codigo = codigo;
+		this.descricao = descricao;
+	}
+	public Integer getCodigo() {
+		return codigo;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public static Prioridade toEnum(Integer cod) {
+		if(cod == null) {
+			return null;
+		}
+		
+		for(Prioridade x : Prioridade.values()) {
+			if(cod.equals(x.getCodigo())) {
+				
+			}
+		}
+		throw new IllegalArgumentException("Prioridade Inválido");
+	}
+	
+}
